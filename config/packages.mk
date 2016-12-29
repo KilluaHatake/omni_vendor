c48fbc6a399c52c51bc8f5d3f63e9432b941e79c
KilluaHatake <chinturocks4u@gmail.com>
2016-12-29 22:39:31 +0000

    omni_vendor

---
+++ config/packages.mk
@@ -0,0 +1,48 @@
+# Additional packages
+ifneq ($(TARGET_LOW_RAM_DEVICE), true)
+PRODUCT_PACKAGES += \
+    Basic \
+    Development
+endif
+
+# Additional apps
+PRODUCT_PACKAGES += \
+    Apollo \
+    MonthCalendarWidget \
+    OpenDelta \
+    OmniSwitch \
+    Chromium \
+    OmniJaws \
+    OmniStyle \
+    QuickSearchBox \
+    OmniClockOSS \
+    MusicFX
+
+
+PRODUCT_PACKAGES += \
+    CellBroadcastReceiver
+
+# Additional tools
+PRODUCT_PACKAGES += \
+    bash \
+    e2fsck \
+    fsck.exfat \
+    htop \
+    lsof \
+    mke2fs \
+    mount.exfat \
+    nano \
+    openvpn \
+    powertop \
+    tune2fs \
+    vim \
+    mkfs.ntfs \
+    mount.ntfs \
+    fsck.ntfs
+
+# Telephony
+#PRODUCT_PACKAGES += \
+ #   telephony-ext
+
+#PRODUCT_BOOT_JARS += \
+  #  telephony-ext
